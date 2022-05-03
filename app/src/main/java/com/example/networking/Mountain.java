a943c95586f70aa55add12e91403a4246f78512f
Christoffer <a18chrkr@gmail.com>
2022-05-03 16:13:43 +0200

    La till getters och setters i min Mountain.java

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -3,7 +3,31 @@ package com.example.networking;
 
 public class Mountain {
 
+private String name;
+private String location;
+private int height;
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
